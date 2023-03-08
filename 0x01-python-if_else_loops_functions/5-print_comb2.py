513c75f1f56051d7fa937515190f03ba868ff470
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-08 01:00:43 +0000

    Print combination 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 100):
     if i == 99:
-        print(f"{i:02d}", end = "\n")
+        print("{:02d}".format(i), end = "\n")
         break
-    print(f"{i:02d}", end = ", ")
+    print("{:02d}".format(i), end = ", ")
