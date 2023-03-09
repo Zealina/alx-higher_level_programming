8ed7958a22887c35eeb0d85326faf12db3f54bf0
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-09 12:37:34 +0000

    Print all args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@
 if __name__ == '__main__':
     from sys import argv
 print("{} arguments:".format(len(argv) - 1))
-for i in range(len(argv)):
+for i in range(1, len(argv)):
     print("{}: {}".format(i, argv[i]))
