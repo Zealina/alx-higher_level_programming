00ff23bd9866017d2201aa45af096abd00b9a74e
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-12 10:28:54 +0000

    Tuple additiion

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if not tuple_a[0]:
-        tuple_a[0] = 0
-    if not tuple_a[1]:
-        tuple_a[1] = 0
-    if not tuple_b[0]:
-        tuple_b[0] = 0
-    if not tuple_b[1]:
-        tuple_b[1] = 0
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
