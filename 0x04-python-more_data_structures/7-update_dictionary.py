a68452272997fddd9f64a95bc6afd1dda4fc84b6
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-16 00:54:59 +0100

    Update diction

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,6 @@ def update_dictionary(a_dictionary, key, value):
     for x in a_dictionary:
         if x == key:
             a_dictionary[x] = value
-        else:
-            a_dictionary += dict(key, value)
+            return a_dictionary
+    a_dictionary[key] = value
     return a_dictionary
