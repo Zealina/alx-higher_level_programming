b91aa694c7cc00fc1a3415cb831e99562d9f3cb2
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-16 01:11:03 +0100

    simople del

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    for x in a_dictionary:
+    for x in list(a_dictionary):
         if x == key:
             a_dictionary[x] = value
-            return a_dictionary
-    a_dictionary[key] = value
+        else:
+            a_dictionary[key] = value
     return a_dictionary
+
