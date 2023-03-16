4f78076bc8e59690d5644b8afc97f308eb39d242
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-16 01:48:58 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-def best_score(a_dictionary)
+def best_score(a_dictionary):
+    bs = None
+    if a_dictionary is None:
+        return None
+    for k in a_dictionary:
+        if bs is None or bs < a_dictionary[k]:
+            bs = a_dictionary[k]
+    return bs
