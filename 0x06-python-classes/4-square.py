1153212b4e2a7d9a881a934333bc98ba95981bfd
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-29 02:57:12 +0000

    Private setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define a square of size 'size'"""
+
+
+class Square:
+    """ This class contains information about a
+    square"""
+    def __init__(self, size=0):
+        """The initialization method of the Square class
+
+        Args:
+            size (int): The size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """ area: Calculate the area of a square from size
+        Return: the Area
+        """
+        return self.__size ** 2
