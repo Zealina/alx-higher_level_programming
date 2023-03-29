6e810d885c179eb6b5f2c6df0db4607738dc508a
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-29 01:48:09 +0000

    ValueError verification

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a square of size 'size'"""
+
+
+class Square:
+    """ This class contains information about a
+    square"""
+    def __init__(self, size=0):
+        """The initialization method of the Square class
+
+        Args:
+            size (int): The size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must ben >= 0')
+        self.__size = size
