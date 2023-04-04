3850d5235171aee149384cb134d80c66c1b05227
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-04 01:30:49 +0000

    Set and get attr for rect

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,39 +3,30 @@
 
 
 class Rectangle:
-    '''Defines the properties of a rectangle
-
-    Args:
-        width (int): the width of the rectangle
-        height (int): the height of the rectangle
-    '''
+    '''Defines the properties of a rectangle'''
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
-    @property
     def width(self):
         '''Getter for width'''
         return self.__width
 
-    @width.setter
     def width(self, value):
         '''Setter for width'''
-        if not isinstance(self.__width, int):
+        if type(value) is not int:
             raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
-        self.__width = value
+        self.__width = width
 
-    @property
     def height(self):
         '''Getter for height'''
         return self.__height
 
-    @height.setter
     def height(self, value):
         '''Setter for height'''
-        if not isinstance(self.__width, int):
+        if type(value) is not int:
             raise TypeError('height must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
