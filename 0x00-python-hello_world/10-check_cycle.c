1260fa56ee0f8c09a84b91f3e04295035c791359
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-05 05:53:01 +0100

    Will work later

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,15 +5,14 @@
  *
  * Return: void
  */
-void freeptr(listp_t **head)
+void freeptr(listp_t *head)
 {
 	listp_t *temp;
 
-	while (*head)
+	while (head)
 	{
-		temp = (*head)->next;
-		free((*head)->ptr);
-		free(*head);
+		temp = head->next;
+		free(head);
 		head = temp;
 	}
 }
@@ -46,6 +45,6 @@ int check_cycle(listint_t *list)
 				return (1);
 		}
 	}
-	freeptr(&headptr);
+	freeptr(headptr);
 	return (0);
 }
