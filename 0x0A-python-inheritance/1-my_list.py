470b8923bb0867d8c3f34ef6888c032490a68576
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-11 21:38:24 +0100

    lookup

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+'''
+Create a Class MyList
+'''
+
+
+class MyList(list):
+    '''Class, a subclass of list'''
+    def __init__(self, my_list=[]):
+        '''Initialize an instance of class'''
+        self.my_list = my_list
+
+    def print_sorted(self):
+        '''Print sorted list'''
+        print(self.my_list.sort())
