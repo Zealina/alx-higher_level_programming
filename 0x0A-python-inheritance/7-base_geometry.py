ba249bca7227a7b5ec1223b553676d26d41cafec
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-12 00:34:25 +0000

    Do not initialize

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Creates class of base geometry'''
+
+
+class BaseGeometry:
+    '''A class called BaseGeometry'''
+
+    def area(self):
+        '''Raises an exception'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''Checks if the value is an integer
+        Args:
+            name (str): The name associated with value
+            value (int) : The value associated with name
+
+        Returns:
+            Nothing, just checks for the value of "value
+        '''
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
