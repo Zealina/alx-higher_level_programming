416916d2b3354b33b3c97d3b9978fa5d0462c4df
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-12 23:45:37 +0000

    append write

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Open and append to a file'''
+
+
+def append_write(filename="", text=""):
+    '''Write text to an open file
+    Args:
+        filename (str): The name of the file
+        text (str): Append into the file
+    Return:
+        The number of written characters
+    '''
+    with open(filename, mode='a', encoding="UTF-8") as myfile:
+        myfile.write(text)
+    return len(text)
