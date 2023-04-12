86d44d471b7adb52db9b9a466968d6da34623326
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-12 23:23:05 +0000

    Print read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@ Read a file and print it to standard output
 
 
 def read_file(filename=""):
-    ''' Read file from filename and print to stdout 
+    ''' Read file from filename and print to stdout
     Args:
         filename (str): The name of the file to be printed
     Return:
