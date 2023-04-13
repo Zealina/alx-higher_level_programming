ab82b364f5c33f786299d4e715faca096a8fb438
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-13 03:12:53 +0000

    Test cases

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-function that inserts a line of text to a file, after each line containing a specific string
+function that inserts a line of text to a file
 """
 
 
@@ -8,6 +8,8 @@ def append_after(filename="", search_string="", new_string=""):
     '''module Search and update
     '''
     with open(filename, 'r+') as f:
+        if !search_string or !new_string:
+            return
         lines = f.readlines()
         i = 0
         for line in lines:
