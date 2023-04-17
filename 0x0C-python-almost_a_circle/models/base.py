fbbc3bb0a9ecb7826d74de4be587de1cb67060d3
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-04-17 22:39:17 +0000

    Back up

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,17 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_obj):
+        """Save json string documentation to file
+        Args:
+            list_obj (object): Contains the list of all instances
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w', encoding='UTF-8') as fd:
+            if list_obj is None or list_obj == []:
+                fd = '[]'
+                return
+            workwith = [work.to_dictionary() for work in list_obj]
+            fd = cls.to_json_string(workwith)
