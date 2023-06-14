94ba00ea529136bf205c5b9a8e1a6e5e962bd93c
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-06-14 20:17:29 +0000

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Module to Read a file"""
 
+
 def read_file(filename=""):
     """
     Function to read a file and print it out
-    
     Args:
         filename (str): The name of the file to be read
     """
