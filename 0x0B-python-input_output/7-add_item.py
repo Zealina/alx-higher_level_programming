8a1f721edc049fd4f7ca3354615e85aad8812c78
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-06-14 23:11:59 +0000

    Figured it out

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,11 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    new_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    new_list = []
+with open('add_item.json', 'r+', encoding='utf-8') as f:
+    test_to_create = f.read()
+new_list = []
+if test_to_create == "":
+    save_to_json_file(new_list, 'add_item.json')
+new_list += load_from_json_file('add_item.json')
 new_list.extend(sys.argv[1:])
 save_to_json_file(new_list, 'add_item.json')
