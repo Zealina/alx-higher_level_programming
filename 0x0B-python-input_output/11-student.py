19acc0088eedf7c5b1b7913af2249af473977758
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-06-15 00:32:10 +0000

    update values of student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module defines a class 'student'
+"""
+
+
+class Student:
+    """
+    This class defines the attributes of a student
+    Args:
+        first_name (str): The firstname of the student
+        last_name (str): The lastname of said student
+        age (int): The age of the student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of the attributes of class
+        'Student'"""
+        my_dict = {}
+        list_of_keys = list(self.__dict__)
+        if type(attrs) is list:
+            for key in attrs:
+                if key in list_of_keys:
+                    my_dict[key] = self.__dict__[key]
+            return my_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Reload the values of the instance from the json dict
+        """
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.key = value
