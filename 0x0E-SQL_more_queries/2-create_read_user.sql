d813402f10bd8e251454bc23fa4c44b43d5b6c8b
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-07-20 02:49:46 +0000

    Grant user 2 select

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,1 +1,7 @@
 -- Create Database and Read New user
+-- Create The Database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create the user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant user_0d_2 the select privilege
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
