6fa1325538c061dd6fe068689d6bc6b711b02dbe
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-07-20 10:35:55 +0000

    flush privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,5 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant User all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- flush privileges
+FLUSH PRIVILEGES;
