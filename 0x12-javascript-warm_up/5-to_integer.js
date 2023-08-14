964a4b021ceb7883da4281213a4b6a6c85127901
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-08-14 17:39:53 +0000

    NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const arg = process.argv;
 const value = parseFloat(arg[2]);
 
 if (!isNaN(value) && arg[2] === value.toString()) {
-  console.log('My number: ' +  Math.floor(value).toString());
+  console.log('My number: ' + Math.floor(value).toString());
 } else {
   console.log('Not a number');
 }
