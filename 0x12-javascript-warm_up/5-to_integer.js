40ff3b096f60fc35a9eeb930652dfe86e3506129
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-11-17 20:43:51 +0000

    Restart

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv;
-const value = parseFloat(arg[2]);
-
-if (!isNaN(value) && arg[2] === value.toString()) {
-  console.log('My number: ' + Math.floor(value).toString());
-} else {
-  console.log('Not a number');
-}
