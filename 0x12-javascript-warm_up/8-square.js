eb4fc4d484086732bd083e67e6a1f979167ac2a1
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-11-18 07:29:30 +0000

    Print Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 const myArg = parseInt(process.argv[2]);
-if (myArg && myArg > 0) {
+if (!myArg) {
+  console.log('Missing size');
+} else if (myArg > 0) {
   for (let i = 0; i < myArg; i++) {
     for (let j = 0; j < myArg; j++) {
       process.stdout.write('X');
