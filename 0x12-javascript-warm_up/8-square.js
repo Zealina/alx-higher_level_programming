78041b589016d0f6b8d97da4b9aed5b1c7a29459
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-11-18 07:15:11 +0000

    Print Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArg = parseInt(process.argv[2]);
+if (myArg && myArg > 0) {
+  for (let i = 0; i < myArg; i++) {
+    for (let j = 0; j < myArg; j++) {
+      process.stdout.write('X');
+    }
+    console.log('');
+  }
+}
