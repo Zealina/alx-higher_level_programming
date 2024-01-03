4945a8ca9f11b1ae201a53221504e6d5f29481b8
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-04 00:46:58 +0100

    Write page to file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, response, body) {
+	if (err) {
+		console.err(err);
+	} else {
+		fs.writeFile(process.argv[3], body, 'utf-8', function (writeErr) {
+			console.error(writeErr);
+		})
+	}
+});
