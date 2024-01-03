2c2b49c199c95a75cb083576e0a5f9a3269be244
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-03 15:36:39 -0800

    Star Wars Count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,10 +3,21 @@
 const request = require('request');
 
 request(process.argv[2], function (err, response, body) {
-	if (err) {
-		console.error(err)
-	} else {
-		console.log('Rspnc: ' + reponse)
-		console.log('Body: ' + body)
-	}
-})
+  if (err) {
+    console.error(err);
+  } else {
+    let achilles = 0;
+    body = JSON.parse(body);
+    const episodes = body.results;
+    for (let i = 0; i < episodes.length; i++) {
+      const chars = episodes[i].characters;
+      for (let j = 0; j < chars.length; j++) {
+        const id = chars[j].slice(-3, -1);
+        if (id === '18') {
+          achilles += 1;
+        }
+      }
+    }
+    console.log(achilles);
+  }
+});
