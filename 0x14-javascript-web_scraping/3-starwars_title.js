dd93da7e576edcd335d32d3968abcdd83bfae493
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-03 14:38:43 -0800

    Star Wars API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,13 @@
 
 const request = require('request');
 
-url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2]
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, function (err, response, body) {
-	if (err) {
-		console.error(err);
-	} else if (body) {
-		let mapping = JSON.parse(body);
-		console.log(mapping);
-	}
+  if (err) {
+    console.error(err);
+  } else if (body) {
+    const mapping = JSON.parse(body);
+    console.log(mapping.title);
+  }
 });
