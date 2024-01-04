f6633acb19714b90ec2f0fe4d21afb00e90b12c5
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-03 17:21:25 -0800

    Characters of a movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,12 +2,21 @@
 
 const request = require('request');
 
-film_url = 'https://swapi-api.alx-tools.com/api/films/';
-request(film_url, function (err, response, body) {
-	if (err) {
-		console.error(err);
-	} else {
-		body = JSON.parse(body)['result'];
-		console.log(body);
-	}
+const filmUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(filmUrl, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    const chars = JSON.parse(body).characters;
+    for (let i = 0; i < chars.length; i++) {
+      request(chars[i], function (err, response, body) {
+        if (err) {
+          console.error(err);
+        } else {
+          body = JSON.parse(body);
+          console.log(body.name);
+        }
+      });
+    }
+  }
 });
