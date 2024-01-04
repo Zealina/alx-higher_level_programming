2685c447ed0e0c79485a3e9296fad8bd77643060
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-03 17:28:34 -0800

    Sort list

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const filmUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(filmUrl, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    const chars = JSON.parse(body).characters;
+    for (let i = 0; i < chars.length; i++) {
+      request(chars[i], function (err, response, body) {
+        if (err) {
+          console.error(err);
+        } else {
+          body = JSON.parse(body);
+          console.log(body.name);
+        }
+      });
+    }
+  }
+});
