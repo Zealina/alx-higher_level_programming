899ac0e66a70fe721b8c3eb71c0849901fa1f9fe
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-04 03:08:48 +0100

    Asynchronous programming

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,13 +3,13 @@
 const request = require('request');
 
 const filmUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(filmUrl, function (err, response, body) {
+request(filmUrl, async function (err, response, body) {
   if (err) {
     console.error(err);
   } else {
     const chars = JSON.parse(body).characters;
     for (let i = 0; i < chars.length; i++) {
-      request(chars[i], function (err, response, body) {
+      await request(chars[i], function (err, response, body) {
         if (err) {
           console.error(err);
         } else {
