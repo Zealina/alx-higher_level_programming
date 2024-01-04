3abf9e3dfbd1a6f7bf53d2d75a3cbe4bd7372d7c
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-03 16:33:20 -0800

    Organize Todos

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,16 +2,22 @@
 
 const request = require('request');
 
-url = 'https://jsonplaceholder.typicode.com/todos';
+const url = 'https://jsonplaceholder.typicode.com/todos';
 request(url, function (err, response, body) {
-	body = JSON.parse(body);
-	let dict = {};
-	for (let i = 0; i < body.length; i++) {
-		if (!dict[body[i]['userId']]) {
-			dict[body[i]['userId']] = 1;
-		} else {
-			dict[body[i]['userId']] += 1;
-		}
-	}
-	console.log(dict);
+  if (err) {
+    console.error(err);
+  } else {
+    body = JSON.parse(body);
+    const dict = {};
+    for (let i = 0; i < body.length; i++) {
+      if (body[i].completed) {
+        if (!dict[body[i].userId]) {
+          dict[body[i].userId] = 1;
+        } else {
+          dict[body[i].userId] += 1;
+        }
+      }
+    }
+    console.log(dict);
+  }
 });
