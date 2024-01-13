1bc5785932fe5fdd4a321d29fe611669f9130a97
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-13 18:45:58 +0000

    Dynamic Page

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('div#add_item').on('click', function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+
+  $('div#remove_item').on('click', function () {
+    $('UL.my_list li:last').remove();
+  });
+
+  $('div#clear_list').on('click', function () {
+    $('UL.my_list').empty();
+  });
+});
